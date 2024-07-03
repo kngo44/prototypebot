2c7d1a132abffa9b765dc791dd55111e827b6260
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-07-02 21:19:21 -0500

    Fixed the libraries and packages

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
+# load the data
+loader = CSVLoader(file_path="interview_questions.csv")
+documents = loader.load()
+
+print(documents[0])
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(documents, embeddings)
